c62021058ee9eead946e59fa67f4465de1437d4b
berivan <berivantaskin4018@gmail.com>
2024-04-06 14:27:20 +0300

    card

--- src/components/lessonCard/LessonCard.jsx
+++ src/components/lessonCard/LessonCard.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Lesson from '../pages/Lesson';
+import {data} from "../../helper/data";
+
 
 const LessonCard = () => {
+    const [lesson, setlesson] = useState(data.lenght)
+    const[lessonData, setLessonData] = useState(
+        data.map((item, index) => ({...item, key: index}))
+    );
+
+    const clear = () => {
+        setLessonData([])
+        setlesson(0);
+    };
+
+
   return (
     <div>
-        <h2></h2>
+        <h2>{lesson} Lessons Today</h2>
+        <Lesson lessonData={lessonData}/>
+        <button onClick={clear}>Clear list</button>
     </div>
   )
 }
