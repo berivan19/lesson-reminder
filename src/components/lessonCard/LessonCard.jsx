87e4d8d9201d4f66959370e19be743760a4df855
berivan <berivantaskin4018@gmail.com>
2024-04-08 16:06:33 +0300

    bitti

--- src/components/lessonCard/LessonCard.jsx
+++ src/components/lessonCard/LessonCard.jsx
@@ -1,33 +1,63 @@
+// import React, { useState } from "react";
+// import Lesson from "../pages/Lesson";
+// import { data } from "../../helper/data";
+// import "./Lesson.css";
+
+// const LessonCard = () => {
+//   const [lesson, setlesson] = useState(data.lenght);
+//   const [lessonData, setLessonData] = useState(
+//     data.map((item, index) => ({ ...item, key: index }))
+//   );
+
+//   const clear = () => {
+//     setLessonData([]);
+//     setlesson(0);
+//   };
+
+//   return (
+//     <div className="allCard">
+//        <div className="cardLesson">
+//       <h2>{lesson} Lessons Today</h2>
+//       <Lesson lessonData={lessonData} />
+//       <button onClick={clear}>Clear list</button>
+//     </div>
+//     </div>
+
+
+
+   
+//   );
+// };
+
+// export default LessonCard;
+
+
+
 import React, { useState } from "react";
 import Lesson from "../pages/Lesson";
+import "./Lesson.css";
 import { data } from "../../helper/data";
-import "./LessonCard.css";
 
 const LessonCard = () => {
-  const [lesson, setlesson] = useState(data.lenght);
+  const [lesson, setLesson] = useState(data.length);
   const [lessonData, setLessonData] = useState(
     data.map((item, index) => ({ ...item, key: index }))
   );
 
   const clear = () => {
     setLessonData([]);
-    setlesson(0);
+    setLesson(0);
   };
 
   return (
-    <div className="allCard">
-       <div className="cardLesson">
-      <h2>{lesson} Lessons Today</h2>
-      <Lesson lessonData={lessonData} />
-      <button onClick={clear}>Clear list</button>
-    </div>
-    </div>
-
-
-
-   
+    <main>
+      <div className="container">
+        <h2>{lesson} lessons Today</h2>
+        <Lesson lessonData={lessonData} />
+        <button onClick={clear}>Clear List</button>
+      </div>
+    </main>
   );
 };
 
 export default LessonCard;
-
--- src/components/pages/Lesson.jsx
+++ src/components/pages/Lesson.jsx
@@ -1,27 +1,58 @@
-import React from 'react';
+// import React from 'react';
 
-const Lesson = ({lessonData}) => {
+// const Lesson = ({lessonData}) => {
+//   return (
+//     <>
+//     {lessonData.map((lesson) => {
+//         return (
+//             <div key={lesson.id}>
+//                 <div>
+//                     <img src={lesson.image} alt={lesson.name} />
+//                 </div>
+//                 <div>
+//                     <p>
+//                         {""}
+//                         <span>Leson Name</span> {lesson.name}
+//                     </p>
+//                     <p>
+//                         {""}
+//                         <span>Lesson Hour</span> {lesson.hour}
+//                     </p>
+//                 </div>
+//             </div>
+//         );
+//     })}
+//     </>
+//   );
+// };
+
+// export default Lesson;
+
+
+import React from "react";
+
+const Lesson = ({ lessonData }) => {
   return (
     <>
-    {lessonData.map((lesson) => {
+      {lessonData.map((lesson) => {
         return (
-            <div key={lesson.id}>
-                <div>
-                    <img src={lesson.image} alt={lesson.name} />
-                </div>
-                <div>
-                    <p>
-                        {""}
-                        <span>Leson Name</span> {lesson.name}
-                    </p>
-                    <p>
-                        {""}
-                        <span>Lesson Hour</span> {lesson.hour}
-                    </p>
-                </div>
+          <div key={lesson.id} className="lesson">
+            <div className="img">
+              <img src={lesson.image} alt={lesson.name} />
             </div>
+            <div className="text">
+              <p>
+                {" "}
+                <span>Lesson Name :</span> {lesson.name}
+              </p>
+              <p>
+                {" "}
+                <span>Lesson Hour :</span> {lesson.hour}
+              </p>
+            </div>
+          </div>
         );
-    })}
+      })}
     </>
   );
 };
